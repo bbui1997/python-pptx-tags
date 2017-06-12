9d67e56325a39c092630d44788eea264c0057f34
bbui1997 <bbui1997@gmail.com>
2017-06-12 19:20:06 -0400

    Updated stopwords

--- pptParser.py
+++ pptParser.py
@@ -5,7 +5,8 @@
 #   c. finalize as a group what we want to be our metadata
 #   d. figure out/edit how the metadata is being inserted to the program
 # 2) Set up code delivery in a way where our user only has to do 1 simple installation to use our program
-
+from __future__ import division, unicode_literals
+from textblob import TextBlob as tb
 import codecs
 import os
 import operator
@@ -13,21 +14,20 @@ from Tkinter import *
 import Tkinter, Tkconstants, tkFileDialog
 from stopwords import filter_stop
 from pptx import Presentation # pip install python-pptx
-
+from tfidfForStringLists import sortedTfIdfLists
+from time import time
 # 1) Open dialog box to select pptx files (currently doesn't parse metadata for .ppt files)
 # 2) For each presentation, parse presentation for text
 # 3) Take text and categorize into core properties
 # 4) Actually insert those core properties to each Presentation
-
+DEFAULT_VAL = 10
 def main():
     # Get all of files that user wants to change tags for
     # Tuple is a ( [list of Presentations], [list of filenames] )
     prsAndFileNameTuple = findFile()
-
-
-
-
-    if prsAndFileNameTuple is None:
+    if prsAndFileNameTuple == ([],[]):
+        print "No pptx presentations found. Exiting program.\n"
+        os.system("pause")
         sys.exit()
 
     else:
@@ -45,32 +45,41 @@ def main():
             wordList = parseText(prs)
 
             # rank the word list to figure out the order in which we will populate the tags
-            metadata = parseMetaData(wordList)
-
-            # Insert metadata (Core Properties) to appropriate location
-            populateCoreProperties(prs, metadata, filename)
+            metadata = parseMetaData(wordList, DEFAULT_VAL)
 
             # Insert metadata (Core Properties) to tags
             populateCoreProperties(prs, metadata, filename)
-
             i += 1
+            print
+
+    print("Successfully added tags to all .pptx files in directory")
     os.system("pause")
 
 def findFile():
     pptx_files = [] # list of filenames
     powerPoints = [] # list of Presentations
 
-    pptx_files = tkFileDialog.askopenfilenames(filetypes = (("Power Point", "*.pptx"),)) # add each file name to list
-
-    count = 0
-    for fileName in pptx_files:
-        print "Selected files " + fileName
-        powerPoints.append(Presentation(fileName))
-        count+=1
-
-    if count == 0:
-        print("No files entered. Aborting")
-        return None
+    # pptx_files = tkFileDialog.askopenfilenames(filetypes = (("Power Point", "*.pptx"),)) # add each file name to list
+    #
+    # count = 0
+    # for fileName in pptx_files:
+    #     print "Selected files " + fileName
+    #     powerPoints.append(Presentation(fileName))
+    #     count+=1
+    #
+    # if count == 0:
+    #     print("No files entered. Aborting")
+    #     return None
+    currentPath = os.path.dirname(os.path.abspath(__file__))
+    print "Finding all .pptx files.."
+    for file in os.listdir(currentPath):
+        try:
+          if file.endswith(".pptx"):
+              pptx_fullPath = os.path.join(currentPath, file)
+              pptx_files.append(pptx_fullPath)
+              powerPoints.append(Presentation(pptx_fullPath))
+        except Exception:
+          print "Error with File:" + pptx_fullPath
 
     # return 2-tuple of lists of Presentations, and filenames
     return (powerPoints, pptx_files)
@@ -80,51 +89,41 @@ def findFile():
 # insert each word to a word list
 # filter out common words such as "the" or "is" and return that word list
 def parseText(presentation):
-    wordList = []
+    splitText = [] # get rid of extra white space
+    text = ""
+    listOfTexts = []
     greatestFontDict = {}
     for slide in presentation.slides:
-        max = 0
-        greatestRun = []
         for shape in slide.shapes:
             if not shape.has_text_frame:
                 continue
             for paragraph in shape.text_frame.paragraphs:
-
                 for run in paragraph.runs:
                     # read the text in the text box, encode it from unicode to ascii, get rid of extra white space
                     # then finally add all the contents in the split() list to the word list
                     # could have data loss
-                    if(run.font.size >= max):
-                        greatestRun = run.text.encode('ascii', 'ignore').split()
-                        max = run.font.size
+                    splitText = filter_stop(run.text.encode('ascii', 'ignore').split())
+                    text += " ".join(splitText)
+        text = text.lower()
+        listOfTexts.append(tb(text))
+    return listOfTexts
 
-        wordList.extend(greatestRun)
-    return filter_stop(wordList)
-
-# find the most common words in the word list
+# find the X most common words in the word list
 # return a String
-def parseMetaData(wordList):
-    # get the word count
-    word_count = {}
-    for word in wordList:
-        if word in word_count:
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
-    sorted_words = sorted(word_count, key=word_count.__getitem__, reverse=True)
-
+def parseMetaData(wordList, val):
+    sorted_words = sortedTfIdfLists(wordList)
     # initialize loop variables
     keywords = ""
-    i = 0
 
     # This if statement is here to get a string with no extra white space
     if len(sorted_words) != 0:
-        keywords = sorted_words[0]
-        i += 1
+        keywords = sorted_words[0][0]
+
     # top 15 or when we run out
-    while i < 15 and i < len(sorted_words):
-        keywords += ", " + sorted_words[i]
-        i += 1
+    for word in sorted_words[1:val]:
+        keywords += ", " + word[0]
+
+    print "Adding tags: " + keywords
 
     return keywords
 
@@ -134,10 +133,20 @@ def parseMetaData(wordList):
 # or how we want to insert it
 def populateCoreProperties(presentation, metadata, filename):
     string = ""
-    for item in metadata:
-        string += item
-    presentation.core_properties.keywords = string
+
+    ## Useless, right now it's a single string anyways
+    # for item in metadata:
+    #     string += item
+    # presentation.core_properties.keywords = string
+    presentation.core_properties.keywords = metadata
     presentation.save(filename)
+
+def printAllKeywords():
+    prsAndFileNameTuple = findFile()
+    for i in range(len(prsAndFileNameTuple[1])):
+        prs = prsAndFileNameTuple[0][i]
+        filename = prsAndFileNameTuple[1][i]
+        print "hello"
 
 if __name__ == "__main__":
     main()
--- stopwords.py
+++ stopwords.py
@@ -1,11 +1,26 @@
 
 def filter_stop(words):
-
-    stop_list = ['all', 'just', 'being', 'over', 'both', 'through', 'yourselves', 'its', 'before', 'herself', 'had', 'should', 'to', 'only', 'under', 'ours', 'has', 'do', 'them', 'his', 'very', 'they',
-            'not', 'during', 'now', 'him', 'nor', 'did', 'this', 'she', 'each', 'further', 'where', 'few', 'because', 'doing', 'some', 'are', 'our', 'ourselves', 'out', 'what', 'for', 'while', 'does', 'above', 'between', 't',
-            'be', 'we', 'who', 'were', 'here', 'hers', 'by', 'on', 'about', 'of', 'against', 's', 'or', 'own', 'into', 'yourself', 'down', 'your', 'from', 'her', 'their', 'there', 'been', 'whom', 'too', 'themselves', 'was',
-            'until', 'more', 'himself', 'that', 'but', 'don', 'with', 'than', 'those', 'he', 'me', 'myself', 'these', 'up', 'will', 'below', 'can', 'theirs', 'my', 'and', 'then', 'is', 'am', 'it', 'an', 'as', 'itself', 'at',
-            'have', 'in', 'any', 'if', 'again', 'no', 'when', 'same', 'how', 'other', 'which', 'you', 'after', 'most', 'such', 'why', 'a', 'off', 'i', 'yours', 'so', 'the', 'having', 'once']
+    # extracted from stop_words import get_stop_words
+    stop_list = ["a", "about", "above", "after", "again", "against", "all", "am",
+    "an", "and", "any", "are", "aren't", "as", "at", "be", "because",
+    "been", "before", "being", "below", "between", "both", "but", "by", "can't",
+    "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't",
+    "doing", "don't", "down", "during", "each", "few", "for", "from", "further",
+    "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd",
+    "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself",
+    "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in",
+    "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more",
+    "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on",
+    "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over",
+    "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should",
+    "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their",
+    "theirs", "them", "themselves", "then", "there", "there's", "these", "they",
+    "they'd", "they'll", "they're", "they've", "this", "those", "through", "to",
+    "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll",
+    "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where",
+    "where's", "which", "while", "who", "who's", "whom", "why", "why's",
+    "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've",
+    "your", "yours", "yourself", "yourselves"]
 
     list = []
     for word in words :
